393473fad58c35aef5a8babd85254a2c12a4a59b
TiedWithType <kamilochman79@gmail.com>
2024-09-26 20:59:03 +0200

    Refactor CustomElement for Enhanced Template Management and Attribute Handling
    - **Updated `attributeChangedCallback`:**
      - Modified the method to now accept the attribute name and its new value as parameters when invoking `updateTemplate`, allowing for more granular updates based on specific attribute changes.
    
    - **Redesigned `updateTemplate`:**
      - Implemented logic to parse the new HTML template using `DOMParser`, allowing for dynamic updates of the innerHTML of elements with matching `data-attr` attributes.
      - Introduced conditional checks to compare the number of new elements against the existing elements:
        - If counts match, innerHTML of current elements is updated selectively.
        - If counts differ or no matching elements are found, the entire template is re-rendered, ensuring the UI is always consistent with the latest state.
    
    - **Removed Redundant Logic:**
      - Eliminated the previous implementation of `updateTemplate` that directly rendered the full template without considering attribute changes, improving performance and responsiveness.
    
    This refactor enhances the efficiency of rendering updates and better aligns the component's behavior with its data-driven nature.

--- @web/custom.element.ts
+++ @web/custom.element.ts
@@ -22,7 +22,7 @@ export class CustomElement extends HTMLElement {
  attributeChangedCallback(attr, prev, next) {
   if(prev !== next) { 
    this[attr] = next;
-   this.updateTemplate();
+   this.updateTemplate(attr, next);
   }
  }
  
@@ -31,13 +31,33 @@ export class CustomElement extends HTMLElement {
  }
  
  templateRender() {
-  this.updateTemplate();
- 
   this.shadow.appendChild(this.styleRef);
   this.shadow.appendChild(this.host);
+  
+  this.host.innerHTML = this.template()
  }
  
- updateTemplate() {
-  this.host.innerHTML = this.template();
- }
+ updateTemplate(attr, next) {
+  if (typeof this.template === 'function') {
+    const parser = new DOMParser();
+    const parsedDocument = parser.parseFromString(this.template(), 'text/html');
+    const newElements = parsedDocument.querySelectorAll(`[data-attr="${attr}"]`);
+
+    if (newElements.length > 0) {
+     const currentElements = this.shadow.querySelectorAll(`[data-attr="${attr}"]`);
+    if (currentElements.length === newElements.length) {
+     
+        currentElements.forEach((currentElement, index) => {
+          currentElement.innerHTML = newElements[index].innerHTML;
+        });
+      } else {
+        this.host.innerHTML = this.template()
+      }
+    } else {  
+      this.host.innerHTML = this.template()
+    }
+  }
+}
+
+
 }
